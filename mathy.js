6833383c66be9c493d161ab294c77a7e7eb2fa27
Ryan <ryanpburnett@yahoo.com>
2021-06-02 18:44:07 -0500

    first commit

---
+++ mathy.js
@@ -0,0 +1,40 @@
+// // lindenmeyer
+
+// let output = ['a'];
+
+// while(output.length < 10) {
+//         if(output === 'a') {
+//             output = 'ab'
+//         }else if(output === 'b'){
+//             output = 'bb'
+//         }
+
+//     console.log(output);
+// }
+
+// // fibonacci
+
+// let prevNum = BigInt('0')
+// let num = BigInt('1')
+// let numOfTerms = 0
+
+// while(numOfTerms < 500) {
+//     console.log(num)
+//     numOfTerms++
+//     let storage = num
+//     num = num + prevNum
+//     prevNum = storage
+// }
+
+// 
+
+// doubler
+
+let num = BigInt('1')
+let numOfTerms = 0
+
+while(numOfTerms < 100) {
+    console.log(num)
+    numOfTerms++
+    num = num*BigInt('2')
+}
